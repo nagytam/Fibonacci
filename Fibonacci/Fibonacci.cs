3cd0ef3529b9b1553b9d5d63c7400a5cc186c3ae
Tamas Nagy <mail@tamas-nagy.net>
2024-11-25 11:35:06 +0100

    Benchmark

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,84 @@
+﻿using BenchmarkDotNet.Attributes;
+
+public class Fibonacci
+{
+    public long FibonacciRecursive(long n)
+    {
+        switch (n)
+        {
+            case 0: return 0;
+            case 1: return 1;
+            default:
+                {
+                    return FibonacciRecursive(n - 2) + FibonacciRecursive(n - 1);
+                }
+        }
+    }
+
+    public static long FibonacciWithoutRecursion(long n)
+    {
+        switch (n)
+        {
+            case 0: return 0;
+            case 1: return 1;
+            default:
+                {
+                    long last2 = 0;
+                    long last1 = 1;
+                    long fibonacci = 0;
+                    for (int i = 2; i <= n; i++)
+                    {
+                        fibonacci = last2 + last1;
+                        last2 = last1;
+                        last1 = fibonacci;
+                    }
+                    return fibonacci;
+                }
+        }
+    }
+
+    public static IEnumerable<long> FibonacciWithYield()
+    {
+        yield return 0;
+        yield return 1;
+
+        long last2 = 0;
+        long last1 = 1;
+        while (true)
+        {
+            var sum = last2 + last1;
+            last2 = last1;
+            last1 = sum;
+            yield return sum;
+        }
+    }
+
+    [Benchmark]
+    public void FibonacciRecursiveBenchmark()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine(FibonacciRecursive(i));
+        }
+    }
+
+    [Benchmark]
+    public void FibonacciWithoutRecursionBenchmark()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine(FibonacciWithoutRecursion(i));
+        }
+    }
+
+    [Benchmark]
+    public void FibonacciWithYieldBenchmark()
+    {
+        var enumerator = FibonacciWithYield().GetEnumerator();
+        for (int i = 0; i < 20; i++)
+        {
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+        }
+    }
+}
--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,57 +1,7 @@
-﻿long Fibonacci(long n)
-{
-    switch (n)
-    {
-        case 0: return 0;
-        case 1: return 1;
-        default:
-            {
-                return Fibonacci(n-2) + Fibonacci(n-1);
-            }
-    }
-}
+﻿using BenchmarkDotNet.Running;
 
-long Fibonacci2(long n)
-{
-    switch (n)
-    {
-        case 0: return 0;
-        case 1: return 1;
-        default:
-            {
-                long last2 = 0;
-                long last1 = 1;
-                long fibonacci = 0;
-                for (int i = 2; i <= n; i++)
-                {
-                    fibonacci = last2 + last1;
-                    last2 = last1;
-                    last1 = fibonacci;
-                }
-                return fibonacci;
-            }
-    }
-}
+var summary = BenchmarkRunner.Run<Fibonacci>();
 
-IEnumerable<long> Fibonacci3()
-{
-    yield return 0;
-
-    long last2 = 0;
-    long last1 = 1;
-    while (true)
-    {
-        var sum = last2 + last1;
-        last2 = last1;
-        last1 = sum;
-        yield return sum;
-    }
-}
-
-//Console.WriteLine(Fibonacci(50));
-//Console.WriteLine(Fibonacci2(50));
-
-foreach (var n in Fibonacci3())
-{
-    Console.WriteLine(n);
-}
+//Fibonacci.FibonacciRecursiveBenchmark();
+//Fibonacci.FibonacciWithoutRecursionBenchmark();
+//Fibonacci.FibonacciWithYieldBenchmark(); 
